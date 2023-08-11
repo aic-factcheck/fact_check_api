b0138edbb5a90ffd0cb439e4be8359640b46777c
rasto <rasto.kopal@gmail.com>
2023-08-11 11:11:32 +0200

    fix: fixed PATCH review

--- src/reviews/dto/update-review.dto.ts
+++ src/reviews/dto/update-review.dto.ts
@@ -1,3 +1,36 @@
-import { CreateReviewDto } from './create-review.dto';
+import {
+  MaxLength,
+  IsString,
+  IsEnum,
+  IsArray,
+  IsUrl,
+  IsOptional,
+} from 'class-validator';
+import { VoteTypes } from '../enums/vote.types';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateReviewDto implements Partial<CreateReviewDto> {}
+export class UpdateReviewDto {
+  @ApiProperty({ example: 'This is the claim that is selected from article.' })
+  @IsOptional()
+  @MaxLength(512)
+  @IsString()
+  text?: string;
+
+  @ApiProperty({ example: 'FALSE' })
+  @IsOptional()
+  @IsEnum(VoteTypes)
+  vote?: VoteTypes;
+
+  @ApiProperty({
+    example: ['https://github.com/aic-factcheck', 'www.google.com'],
+  })
+  @IsOptional()
+  @IsArray()
+  @IsUrl({}, { each: true })
+  links?: string[];
+
+  @ApiProperty({ example: 'en' })
+  @IsOptional()
+  @MaxLength(32)
+  lang?: string;
+}
